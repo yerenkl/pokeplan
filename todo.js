d65fbf296a7e4c495f6ed4c4d3f36aaad4b4d483
yeren <yerenkl@gmail.com>
2023-03-14 01:16:09 +0300

    end button added

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const addForm = document.querySelector("form.add");
 const ul = document.querySelector("ul.todos");
-
+var modal = document.getElementById('myModal');
+var span = document.getElementById("close");
 // Get the todo list from local storage
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -10,7 +11,7 @@ const renderTodos = () => {
   todos.forEach((todo) => {
     const html = `
       <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${todo}</span>
+        <span onclick="strike(this)">${todo}</span>
         <i class="far fa-trash-alt delete"></i>
       </li>
     `;
@@ -18,6 +19,26 @@ const renderTodos = () => {
   });
 };
 
+function cls() {
+  modal.style.display = "none";
+}
+
+const btn = () => {
+  modal.style.display = "block";
+  const total = todos.length;
+  const striked = ul.querySelectorAll("span[style='text-decoration: line-through;']").length;
+  const notStriked = total - striked;
+  if(total>0 && notStriked==0){
+    document.getElementById("tasks").innerHTML="YOU CAUGHT IT!"
+  }
+  else{
+    document.getElementById("tasks").innerHTML="YOU STILL GOT TASKS TO DO!"
+  }
+};
+
+function strike(elem) {
+  elem.style.textDecoration = "line-through";
+}
 // Add new todo
 const handleAddItem = (inputValue) => {
   todos.push(inputValue);
